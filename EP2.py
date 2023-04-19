ebd5f322456ca217969f0561a64df3f83301f8d2
leonardoct3 <leocarvalhoteixeira@gmail.com>
2023-04-19 13:04:57 -0300

    Preenche Frota

--- EP2.py
+++ EP2.py
@@ -6,3 +6,8 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
         elif orientacao=='horizontal':
             lista.append([linha,coluna+e])
     return lista
+def preenche_frota(frota,nome,linha,coluna,orientacao,tamanho):
+    if nome not in frota.keys():
+        frota[nome]=[]
+    frota[nome].append(define_posicoes(linha,coluna,orientacao,tamanho))
+    return frota
