1d7aa86cc1df923f3c53006a69ab3bc75767d478
leonardoct3 <leocarvalhoteixeira@gmail.com>
2023-04-19 12:49:09 -0300

    Define Posições

--- EP2.py
+++ EP2.py
@@ -1,3 +1,9 @@
-print(2+2)
-print(2+2)
-print(4)
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    lista=[]
+    for e in range (tamanho):
+        if orientacao=='vertical':
+            lista.append([linha+e,coluna])
+        elif orientacao=='horizontal':
+            lista.append([linha,coluna+e])
+    return lista
+print(define_posicoes(2,4,'vertical',3))
