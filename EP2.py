e784a9aedf4f16595e85b1697b653d0809af11d8
pedroefnf_arquivos <pedroefnfranca@gmail.com>
2023-04-25 09:29:03 -0300

    Posição Válida

--- EP2.py
+++ EP2.py
@@ -35,5 +35,27 @@ def posiciona_frota(info_navios):
           for i in lista:
             tabuleiro[i[0]][i[1]]=1
     return tabuleiro
-
+def afundados(frota,tabuleiro):
+    x=0
+    for nome in frota.keys():
+        for lugares in frota[nome]:
+            y=0
+            for lugar in lugares:
+                if tabuleiro[lugar[0]][lugar[1]]=='X':
+                    y+=1
+            if y==len(lugares):
+                x+=1
+    return x
+def posicao_valida (infos,linha,coluna,orientacao,tamanho):
+    pos_nav = define_posicoes(linha,coluna,orientacao,tamanho)
+    for i in pos_nav:
+        if i[0] > 9 or i[1]>9:
+            return False
+        for posicoes in infos.values():
+            for f in posicoes:
+                for e in f:
+                    if i[0] == e[0] and i[1] == e[1]:
+                        return False
+                    
+    return True
 
