f9b0eb27aa795b423e37800bb7da7ed1f5bf0f6b
leonardoct3 <leocarvalhoteixeira@gmail.com>
2023-04-27 13:23:03 -0300

    Jogadas do Jogador

--- EP2.py
+++ EP2.py
@@ -1,7 +1,9 @@
 def define_posicoes(linha,coluna,orientacao,tamanho):
     lista=[]
     for e in range (tamanho):
-        if orientacao=='vertical':
+        if orientacao==0:
+            lista.append([linha,coluna])
+        elif orientacao=='vertical':
             lista.append([linha+e,coluna])
         elif orientacao=='horizontal':
             lista.append([linha,coluna+e])
@@ -98,7 +100,68 @@ for nome in navios:
     d = define_posicoes(linha,coluna,orientacao,tamanhos[nome])
     frota_preenchida = preenche_frota(frota,nome,linha,coluna,orientacao,tamanhos[nome])
 
-print(frota_preenchida)
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+
+tabuleiro_oponente=posiciona_frota(frota_oponente)
+
+tabuleiro_jogador=posiciona_frota(frota_preenchida)
+
+jogando = True
+
+def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+    texto = ''
+    texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+    texto += '_______________________________      _______________________________\n'
+
+    for linha in range(len(tabuleiro_jogador)):
+        jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
+        oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+        texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+    return texto
+
+passadas=[]
+
+while jogando:
+    print(monta_tabuleiros(tabuleiro_jogador,tabuleiro_oponente))
+    ja_tem=True 
+    while ja_tem:
+        linha=int(input('Linha?: '))
+        while linha<0 or linha>9:
+            print('Linha inválida')
+            linha=int(input('Linha?: '))
+        coluna=int(input('Coluna?: '))
+        while coluna<0 or coluna>9:
+            print('Coluna inválida')
+            coluna=int(input('Coluna?: '))
+        if [linha, coluna] not in passadas:
+            passadas.append([linha,coluna])
+            ja_tem=False 
+        else:
+            print('A posição linha {0} e coluna {1} já foi informada anteriormente!'.format(linha,coluna))
+    jogada=faz_jogada(tabuleiro_oponente,linha,coluna)
+    afundado=afundados(frota_oponente,tabuleiro_oponente)
+    if afundado==10:
+        print('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando=False
         
     
         
